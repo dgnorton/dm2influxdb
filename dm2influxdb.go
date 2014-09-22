ddc8eb2ef97660a507568d5490692ee17b2d30fc
David Norton <dgnorton@gmail.com>
2014-09-22 17:28:15 -0400

    Add -m cmd line option for max records to insert

--- dm2influxdb.go
+++ dm2influxdb.go
@@ -3,68 +3,117 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"os/user"
 	"github.com/dgnorton/dmapi"
 	influx "github.com/influxdb/influxdb/client"
+	"log"
+	"os/user"
 )
 
 func main() {
-	dmuser := flag.String("u", "", "dailymile username")
+	// parse command line options
+	var dmUser string
+	var maxRecords int
+	flag.StringVar(&dmUser, "u", "", "dailymile username")
+	flag.IntVar(&maxRecords, "m", -1, "max number of records to insert into database")
 	flag.Parse()
-	if *dmuser == "" {
+	if dmUser == "" {
 		flag.Usage()
 		return
 	}
-	osuser, err := user.Current()
+	osUser, err := user.Current()
 	fatalIfErr(err)
-	dmdata := fmt.Sprintf("%s/.dailymile_cli/%s/entries.json", osuser.HomeDir, *dmuser)
-	entries, err := dmapi.LoadEntries(dmdata)
+
+	// load local dailymile data for user specified on command line
+	dmData := fmt.Sprintf("%s/.dailymile_cli/%s/entries.json", osUser.HomeDir, dmUser)
+	entries, err := dmapi.LoadEntries(dmData)
 	fatalIfErr(err)
+
+	// create dailymile database in InfluxDB, if it doesn't exist
 	cfg := &influx.ClientConfig{Username: "root", Password: "root", Database: "dailymile"}
 	client, err := influx.NewClient(cfg)
 	fatalIfErr(err)
-	dblist, err := client.GetDatabaseList()
+	dbList, err := client.GetDatabaseList()
 	fatalIfErr(err)
-	if !dbExists(cfg.Database, dblist) {
+	if !dbExists(cfg.Database, dbList) {
 		fmt.Printf("Creating database: %s\n", cfg.Database)
 		err = client.CreateDatabase(cfg.Database)
 		fatalIfErr(err)
+	} else {
+		fmt.Printf("Database already exists: %s\n", cfg.Database)
 	}
-	series, err := entries2Series(*dmuser, entries)
+
+	// drop existing series for user if it already exists
+	_, err = client.Query(fmt.Sprintf("drop series %s.distance", dmUser))
 	fatalIfErr(err)
-	//fmt.Println(series)
-	err = client.WriteSeriesWithTimePrecision([]*influx.Series{series}, influx.Second)
+	_, err = client.Query(fmt.Sprintf("drop series %s.duration", dmUser))
+	fatalIfErr(err)
+	_, err = client.Query(fmt.Sprintf("drop series %s.pace", dmUser))
+	fatalIfErr(err)
+
+	series, err := entries2Series(dmUser, entries, maxRecords)
+	fatalIfErr(err)
+	err = client.WriteSeriesWithTimePrecision(series, influx.Second)
 	fatalIfErr(err)
 }
 
-func entries2Series(dmuser string, entries *dmapi.Entries) (*influx.Series, error) {
-	series := &influx.Series{
-		Name: dmuser,
-		Columns: []string{"time", "distance", "duration", "title"},
-		Points: [][]interface{}{},
+func entries2Series(dmUser string, entries *dmapi.Entries, maxRecords int) ([]*influx.Series, error) {
+	distanceSeries := &influx.Series{
+		Name:    fmt.Sprintf("%s.distance", dmUser),
+		Columns: []string{"time", "distance"},
+		Points:  [][]interface{}{},
 	}
 
-	for i, entry := range entries.Entries {
-		//if len(entries.Entries) > 500 && i < len(entries.Entries) - 500 {
-		if i > 300 {
-			break // only store the last 500 entries
+	durationSeries := &influx.Series{
+		Name:    fmt.Sprintf("%s.duration", dmUser),
+		Columns: []string{"time", "duration"},
+		Points:  [][]interface{}{},
+	}
+
+	paceSeries := &influx.Series{
+		Name:    fmt.Sprintf("%s.pace", dmUser),
+		Columns: []string{"time", "pace", "paceStr"},
+		Points:  [][]interface{}{},
+	}
+
+	recordCnt := 0
+	for _, entry := range entries.Entries {
+		if entry.Workout.Type == "" {
+			continue
 		}
+		if maxRecords > -1 && recordCnt >= maxRecords {
+			break
+		}
+		recordCnt++
 		tm, err := entry.Time()
 		if err != nil {
 			return nil, err
 		}
-		dist := entry.Workout.Distance.Value
-		dur := entry.Workout.Duration().Seconds()
-		title := entry.Workout.Title
-		point := []interface{}{float64(tm.Unix()), dist, dur, title}
-		series.Points = append(series.Points, point)
+		distance := entry.Workout.Distance.Value
+		point := []interface{}{float64(tm.Unix()), distance}
+		distanceSeries.Points = append(distanceSeries.Points, point)
+
+		duration := entry.Workout.Duration().Seconds() / 60
+		point = []interface{}{float64(tm.Unix()), duration}
+		durationSeries.Points = append(durationSeries.Points, point)
+
+		if distance > 0 && duration > 0 {
+			p, err := entry.Workout.Pace()
+			if err != nil {
+				fmt.Println("pace error")
+				continue
+			}
+			paceStr := dmapi.DurationStr(p)
+			pace := p.Seconds() / 60
+
+			point = []interface{}{float64(tm.Unix()), pace, paceStr}
+			paceSeries.Points = append(paceSeries.Points, point)
+		}
 	}
-	return series, nil
+	return []*influx.Series{distanceSeries, durationSeries, paceSeries}, nil
 }
 
-func dbExists(dbname string, dblist []map[string]interface{}) bool {
-	for _, db := range dblist {
+func dbExists(dbname string, dbList []map[string]interface{}) bool {
+	for _, db := range dbList {
 		if dbname == db["name"] {
 			return true
 		}
